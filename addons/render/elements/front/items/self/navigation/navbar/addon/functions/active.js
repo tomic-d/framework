f6722d675f11abc03a5c6710f0124c4dac71c7e8
Dejan Tomic <hi@iamdejan.com>
2026-02-20 19:00:33 +0100

    remove built-in elements from elements addon, bump version to 2.0.6

--- addons/render/elements/front/items/self/navigation/navbar/addon/functions/active.js
+++
@@ -1,7 +0,0 @@
-navbar.Fn('active', function(id)
-{
-	Object.values(navbar.Items()).forEach(item =>
-	{
-		item.Set('active', item.Get('id') === id);
-	});
-});
--- addons/render/elements/front/items/self/navigation/sidebar/addon/functions/active.js
+++
@@ -1,7 +0,0 @@
-sidebar.Fn('active', function(id)
-{
-	Object.values(sidebar.Items()).forEach(item =>
-	{
-		item.Set('active', item.Get('id') === id);
-	});
-});
