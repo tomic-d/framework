d26ce905882dae87fccdaa696751b4ac364287b9
Dejan Tomic <hi@iamdejan.com>
2026-02-21 17:11:25 +0100

    wrap cross-addon ItemAdd calls in AddonReady

--- addons/core/commands/front/directives/submit.js
+++ addons/core/commands/front/directives/submit.js
@@ -1,139 +1,139 @@
-directives.ItemAdd({
-	id: 'dh-command-submit',
-	icon: 'terminal',
-	name: 'Command Submit',
-	description: 'Submit form data to a command via commands.Fn',
-	category: 'data',
-	trigger: 'node',
-	order: 665,
-	strict: false,
-	tag: 'dh-command-submit',
-	attributes: {
-		'command': ['string', null, true],
-		'bind': ['string', 'command'],
-		'_success': ['function'],
-		'_error': ['function'],
-		'reset': ['boolean', false],
-		'stop': ['boolean', false],
-		'data': ['object', {}],
-		'api': ['boolean', false]
-	},
-	code: function(data, item, compile, node, identifier)
-	{
-		const config = {};
-		const methods = {};
-
-		methods.init = () =>
-		{
-			methods.config();
-			methods.state();
-			methods.element();
-			methods.handler();
-		};
-
-		methods.state = () =>
-		{
-			if(!compile.data[config.bind])
-			{
-				compile.data[config.bind] = {
-					response: null,
-					error: null,
-					loading: false
-				};
-			}
-		};
-
-		methods.config = () =>
-		{
-			config.command = data['command'].value;
-			config.bind = data['bind'].value;
-			config.onSuccess = data['_success'].value;
-			config.onError = data['_error'].value;
-			config.reset = data['reset'].value;
-			config.stop = data['stop'].value;
-			config.data = data['data'].value;
-			config.api = data['api'].value;
-		};
-
-		methods.element = () =>
-		{
-			config.form = document.createElement('form');
-			config.form.setAttribute('autocomplete', 'off');
-
-			while(node.firstChild)
-			{
-				config.form.appendChild(node.firstChild);
-			}
-
-			node.appendChild(config.form);
-			divhunt.FormSet(config.form, config.data);
-		};
-
-		methods.handler = () =>
-		{
-			config.form.dhCommandSubmit = async (event) =>
-			{
-				event.preventDefault();
-
-				if(config.stop)
-				{
-					event.stopPropagation();
-				}
-
-				await methods.submit();
-			};
-		};
-
-		methods.submit = async () =>
-		{
-			compile.data[config.bind];
-
-			if(compile.data[config.bind].loading)
-			{
-				return;
-			}
-
-			compile.data[config.bind].loading = true;
-			compile.data[config.bind].error = null;
-
-			console.log(compile.data[config.bind]);
-
-			compile.data.Update();
-
-			try
-			{
-				const formData = divhunt.FormGet(config.form);
-				const result = config.api
-					? await commands.Fn('api', config.command, formData)
-					: await commands.Fn('run', config.command, formData);
-
-				compile.data[config.bind].response = result;
-				compile.data[config.bind].error    = null;
-				compile.data[config.bind].loading  = false;
-
-				config.reset && config.form.reset();
-				config.onSuccess && config.onSuccess(compile.data[config.bind]);
-			}
-			catch(error)
-			{
-				compile.data[config.bind].response = null;
-				compile.data[config.bind].error    = error.message;
-				compile.data[config.bind].loading  = false;
-
-				config.onError && config.onError(compile.data[config.bind]);
-			}
-			finally
-			{
-				compile.data.Update();
-			}
-		};
-
-		methods.init();
-	}
-});
-
 divhunt.AddonReady('directives', function()
 {
+	directives.ItemAdd({
+		id: 'dh-command-submit',
+		icon: 'terminal',
+		name: 'Command Submit',
+		description: 'Submit form data to a command via commands.Fn',
+		category: 'data',
+		trigger: 'node',
+		order: 665,
+		strict: false,
+		tag: 'dh-command-submit',
+		attributes: {
+			'command': ['string', null, true],
+			'bind': ['string', 'command'],
+			'_success': ['function'],
+			'_error': ['function'],
+			'reset': ['boolean', false],
+			'stop': ['boolean', false],
+			'data': ['object', {}],
+			'api': ['boolean', false]
+		},
+		code: function(data, item, compile, node, identifier)
+		{
+			const config = {};
+			const methods = {};
+
+			methods.init = () =>
+			{
+				methods.config();
+				methods.state();
+				methods.element();
+				methods.handler();
+			};
+
+			methods.state = () =>
+			{
+				if(!compile.data[config.bind])
+				{
+					compile.data[config.bind] = {
+						response: null,
+						error: null,
+						loading: false
+					};
+				}
+			};
+
+			methods.config = () =>
+			{
+				config.command = data['command'].value;
+				config.bind = data['bind'].value;
+				config.onSuccess = data['_success'].value;
+				config.onError = data['_error'].value;
+				config.reset = data['reset'].value;
+				config.stop = data['stop'].value;
+				config.data = data['data'].value;
+				config.api = data['api'].value;
+			};
+
+			methods.element = () =>
+			{
+				config.form = document.createElement('form');
+				config.form.setAttribute('autocomplete', 'off');
+
+				while(node.firstChild)
+				{
+					config.form.appendChild(node.firstChild);
+				}
+
+				node.appendChild(config.form);
+				divhunt.FormSet(config.form, config.data);
+			};
+
+			methods.handler = () =>
+			{
+				config.form.dhCommandSubmit = async (event) =>
+				{
+					event.preventDefault();
+
+					if(config.stop)
+					{
+						event.stopPropagation();
+					}
+
+					await methods.submit();
+				};
+			};
+
+			methods.submit = async () =>
+			{
+				compile.data[config.bind];
+
+				if(compile.data[config.bind].loading)
+				{
+					return;
+				}
+
+				compile.data[config.bind].loading = true;
+				compile.data[config.bind].error = null;
+
+				console.log(compile.data[config.bind]);
+
+				compile.data.Update();
+
+				try
+				{
+					const formData = divhunt.FormGet(config.form);
+					const result = config.api
+						? await commands.Fn('api', config.command, formData)
+						: await commands.Fn('run', config.command, formData);
+
+					compile.data[config.bind].response = result;
+					compile.data[config.bind].error    = null;
+					compile.data[config.bind].loading  = false;
+
+					config.reset && config.form.reset();
+					config.onSuccess && config.onSuccess(compile.data[config.bind]);
+				}
+				catch(error)
+				{
+					compile.data[config.bind].response = null;
+					compile.data[config.bind].error    = error.message;
+					compile.data[config.bind].loading  = false;
+
+					config.onError && config.onError(compile.data[config.bind]);
+				}
+				finally
+				{
+					compile.data.Update();
+				}
+			};
+
+			methods.init();
+		}
+	});
+
 	document.addEventListener('submit', function(event)
 	{
 		let node = event.target;
--- addons/render/pages/front/items/directives/change.js
+++ addons/render/pages/front/items/directives/change.js
@@ -1,38 +1,41 @@
-directives.ItemAdd({
-	id: 'dh-page',
-	icon: 'file',
-	name: 'Page',
-	description: 'Navigate to a page on render',
-	category: 'navigation',
-	trigger: 'node',
-	order: 666,
-	strict: false,
-	tag: 'dh-page',
-	attributes: {
-		'route': ['string', null, true],
-		'parameters': ['object', {}],
-		'history': ['boolean', true],
-		'timeout': ['number', 0]
-	},
-	code: function(data, item, compile, node, identifier)
-	{
-		const route = data['route'].value;
-		const parameters = data['parameters'].value;
-		const push = data['history'].value;
-		const timeout = data['timeout'].value;
+divhunt.AddonReady('directives', function()
+{
+	directives.ItemAdd({
+		id: 'dh-page',
+		icon: 'file',
+		name: 'Page',
+		description: 'Navigate to a page on render',
+		category: 'navigation',
+		trigger: 'node',
+		order: 666,
+		strict: false,
+		tag: 'dh-page',
+		attributes: {
+			'route': ['string', null, true],
+			'parameters': ['object', {}],
+			'history': ['boolean', true],
+			'timeout': ['number', 0]
+		},
+		code: function(data, item, compile, node, identifier)
+		{
+			const route = data['route'].value;
+			const parameters = data['parameters'].value;
+			const push = data['history'].value;
+			const timeout = data['timeout'].value;
 
-		const change = () =>
-		{
-			pages.Fn('change', route, parameters, { path: true, push });
-		};
+			const change = () =>
+			{
+				pages.Fn('change', route, parameters, { path: true, push });
+			};
 
-		if(timeout > 0)
-		{
-			setTimeout(change, timeout);
+			if(timeout > 0)
+			{
+				setTimeout(change, timeout);
+			}
+			else
+			{
+				change();
+			}
 		}
-		else
-		{
-			change();
-		}
-	}
+	});
 });
