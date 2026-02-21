a81275bf3fb0efce06b5f294ac6a93e87e5286b6
Dejan Tomic <hi@iamdejan.com>
2026-02-21 17:11:25 +0100

    wrap cross-addon ItemAdd calls in AddonReady

--- addons/core/commands/front/directives/run.js
+++ addons/core/commands/front/directives/run.js
@@ -1,90 +1,92 @@
-directives.ItemAdd({
-	id: 'dh-command',
-	icon: 'terminal',
-	name: 'Command',
-	description: 'Execute a command instantly on render',
-	category: 'data',
-	trigger: 'node',
-	order: 664,
-	strict: false,
-	tag: 'dh-command',
-	attributes: {
-		'command': ['string', null, true],
-		'bind': ['string', 'command'],
-		'_success': ['function'],
-		'_error': ['function'],
-		'data': ['object', {}],
-		'api': ['boolean', false]
-	},
-	code: function(data, item, compile, node, identifier)
-	{
-		if(node.tagName.toLowerCase() !== 'dh-command')
+divhunt.AddonReady('directives', function()
+{
+	directives.ItemAdd({
+		id: 'dh-command',
+		icon: 'terminal',
+		name: 'Command',
+		description: 'Execute a command instantly on render',
+		category: 'data',
+		trigger: 'node',
+		order: 664,
+		strict: false,
+		tag: 'dh-command',
+		attributes: {
+			'command': ['string', null, true],
+			'bind': ['string', 'command'],
+			'_success': ['function'],
+			'_error': ['function'],
+			'data': ['object', {}],
+			'api': ['boolean', false]
+		},
+		code: function(data, item, compile, node, identifier)
 		{
-			return;
-		}
-
-		const config = {};
-		const methods = {};
-
-		methods.init = () =>
-		{
-			methods.config();
-
-			if(compile.data[config.bind] !== undefined)
+			if(node.tagName.toLowerCase() !== 'dh-command')
 			{
 				return;
 			}
 
-			compile.data[config.bind] = null;
-			methods.run();
-		};
+			const config = {};
+			const methods = {};
 
-		methods.config = () =>
-		{
-			config.command = data['command'].value;
-			config.bind = data['bind'].value;
-			config.onSuccess = data['_success'].value;
-			config.onError = data['_error'].value;
-			config.data = data['data'].value;
-			config.api = data['api'].value;
-		};
+			methods.init = () =>
+			{
+				methods.config();
 
-		methods.run = async () =>
-		{
-			const state = {
-				response: null,
-				error: null,
-				loading: true
+				if(compile.data[config.bind] !== undefined)
+				{
+					return;
+				}
+
+				compile.data[config.bind] = null;
+				methods.run();
 			};
 
-			try
+			methods.config = () =>
 			{
-				const result = config.api
-					? await commands.Fn('api', config.command, config.data)
-					: await commands.Fn('run', config.command, config.data);
+				config.command = data['command'].value;
+				config.bind = data['bind'].value;
+				config.onSuccess = data['_success'].value;
+				config.onError = data['_error'].value;
+				config.data = data['data'].value;
+				config.api = data['api'].value;
+			};
 
-				state.response = result;
-				state.error    = null;
-				state.loading  = false;
-
-				config.onSuccess && config.onSuccess(state);
-			}
-			catch(error)
+			methods.run = async () =>
 			{
-				state.response = null;
-				state.error    = error.message;
-				state.loading  = false;
+				const state = {
+					response: null,
+					error: null,
+					loading: true
+				};
 
-				config.onError && config.onError(state);
-			}
-			finally
-			{
-				
-				compile.data[config.bind] = state;
-				compile.data.Update();
-			}
-		};
+				try
+				{
+					const result = config.api
+						? await commands.Fn('api', config.command, config.data)
+						: await commands.Fn('run', config.command, config.data);
 
-		methods.init();
-	}
+					state.response = result;
+					state.error    = null;
+					state.loading  = false;
+
+					config.onSuccess && config.onSuccess(state);
+				}
+				catch(error)
+				{
+					state.response = null;
+					state.error    = error.message;
+					state.loading  = false;
+
+					config.onError && config.onError(state);
+				}
+				finally
+				{
+					compile.data[config.bind] = state;
+					compile.data.Update();
+				}
+			};
+
+			methods.init();
+		}
+	});
 });
