2492b2ae4392d464bd0ca23eabecceda3d840fbf
Dejan Tomic <hi@iamdejan.com>
2026-02-24 20:24:12 +0100

    centralized assets system, shared elements/transforms, reorganize addons
    - Add onetype.Assets() centralized asset registration (lib/src/mixins/assets.js)
    - Create lib/assets.js as single source for all asset path mappings
    - Move assets addon from render/ to core/
    - Add shared UI elements (form, global, sections) in lib/items/elements/
    - Add shared transforms in lib/items/transforms/
    - Add shared styles (variables, reset, queries, layout, spacing)
    - Reorganize transforms addon (flatten front/js structure)
    - Remove unused render/items addon
    - Fix addon store capitalization (Store -> store)
    - Rename params element to parameters

--- addons/render/items/js/functions/count.js
+++
@@ -1,9 +0,0 @@
-items.Fn('count', function(group = null)
-{
-	if(!group)
-	{
-		return Object.keys(this.Items()).length;
-	}
-
-	return Object.values(this.Items()).filter(item => item.Get('group') === group).length;
-});
