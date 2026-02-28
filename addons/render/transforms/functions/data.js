c947ee624458af483d77e6e7b5c09665f529ac21
Dejan Tomic <hi@iamdejan.com>
2026-02-28 13:31:19 +0100

    transforms: use ot- prefix for config attributes to avoid HTML conflicts

--- addons/render/transforms/functions/data.js
+++ addons/render/transforms/functions/data.js
@@ -6,14 +6,15 @@ transforms.Fn('data', function(config, node)
     {
         name = name.toLowerCase();
 
-        const raw = node.getAttribute(name);
+        const attribute = 'ot-' + name;
+        const raw = node.getAttribute(attribute);
         const value = raw !== null ? onetype.Function(raw, {}, false) : undefined;
 
         data[name] = onetype.DataDefineOne(typeof value === 'undefined' ? raw : value, definition);
 
         if(raw !== null)
         {
-            node.removeAttribute(name);
+            node.removeAttribute(attribute);
         }
     });
 
